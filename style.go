3b9d966ede1d2ec3b5b298bef81686cefb6a4fce
Marius Merschformann <marius.merschformann@gmail.com>
2025-05-27 00:43:37 +0200

    Almost working linter

--- style.go
+++ style.go
@@ -1,7 +1,10 @@
 package linters
 
 import (
+	"fmt"
 	"go/ast"
+	"go/token"
+	"regexp"
 	"strings"
 
 	"golang.org/x/tools/go/analysis"
@@ -13,21 +16,93 @@ var KeyStyleAnalyzer = &analysis.Analyzer{
 	Run:  run,
 }
 
+// KeyStyle defines the style of keys in maps.
+type KeyStyle string
+
+const (
+	CamelCase  KeyStyle = "camelCase"
+	PascalCase KeyStyle = "PascalCase"
+	KebabCase  KeyStyle = "kebab-case"
+	SnakeCase  KeyStyle = "snake_case"
+)
+
+var camelCaseRegex = regexp.MustCompile("^[a-z]+([A-Z][a-z]*[0-9]*)*$")
+var pascalCaseRegex = regexp.MustCompile("^([A-Z][a-z0-9]+)+$")
+var kebabCaseRegex = regexp.MustCompile("^[a-z]+(-[a-z0-9]+)*$")
+var snakeCaseRegex = regexp.MustCompile("^[a-z]+(_[a-z0-9]+)*$")
+
+func checkStyle(s string, style *regexp.Regexp) bool {
+	return style.MatchString(s)
+}
+
 func run(pass *analysis.Pass) (interface{}, error) {
+	// TODO: get the configuration.
+	style := CamelCase // This should be replaced with actual configuration retrieval logic
+	var styleRegex *regexp.Regexp
+	switch style {
+	case CamelCase:
+		styleRegex = camelCaseRegex
+	case PascalCase:
+		styleRegex = pascalCaseRegex
+	case KebabCase:
+		styleRegex = kebabCaseRegex
+	case SnakeCase:
+		styleRegex = snakeCaseRegex
+	default:
+		return nil, fmt.Errorf("unknown style: %s", style)
+	}
+
 	for _, file := range pass.Files {
 		ast.Inspect(file, func(n ast.Node) bool {
-			if comment, ok := n.(*ast.Comment); ok {
-				if strings.HasPrefix(comment.Text, "// TODO:") || strings.HasPrefix(comment.Text, "// TODO():") {
-					pass.Report(analysis.Diagnostic{
-						Pos:            comment.Pos(),
-						End:            0,
-						Category:       "todo",
-						Message:        "TODO comment has no author",
-						SuggestedFixes: nil,
-					})
+			// Check for composite literals
+			if cl, ok := n.(*ast.CompositeLit); ok {
+				// Check if the composite literal is of type LogData or *LogData or logger.LogData
+				var isLogData bool
+				switch t := cl.Type.(type) {
+				case *ast.Ident:
+					isLogData = t.Name == "LogData"
+				case *ast.StarExpr:
+					if ident, ok := t.X.(*ast.Ident); ok {
+						isLogData = ident.Name == "LogData"
+					} else if sel, ok := t.X.(*ast.SelectorExpr); ok {
+						isLogData = sel.Sel.Name == "LogData"
+					}
+				case *ast.SelectorExpr:
+					isLogData = t.Sel.Name == "LogData"
+				}
+
+				if isLogData {
+					// Check each key in the composite literal
+					for _, elt := range cl.Elts {
+						if kv, ok := elt.(*ast.KeyValueExpr); ok {
+							if keyIdent, ok := kv.Key.(*ast.Ident); ok {
+								if !checkStyle(keyIdent.Name, styleRegex) {
+									pass.Report(analysis.Diagnostic{
+										Pos:            keyIdent.Pos(),
+										End:            0,
+										Category:       "keystyle",
+										Message:        fmt.Sprintf("Key '%s' style should be camelCase", keyIdent.Name),
+										SuggestedFixes: nil,
+									})
+									return false // Stop inspecting this node
+								}
+							} else if keyBasicLit, ok := kv.Key.(*ast.BasicLit); ok && keyBasicLit.Kind == token.STRING {
+								key := strings.Trim(keyBasicLit.Value, `"`)
+								if !checkStyle(key, styleRegex) {
+									pass.Report(analysis.Diagnostic{
+										Pos:            keyBasicLit.Pos(),
+										End:            0,
+										Category:       "keystyle",
+										Message:        fmt.Sprintf("Key '%s' style should be camelCase", key),
+										SuggestedFixes: nil,
+									})
+									return false // Stop inspecting this node
+								}
+							}
+						}
+					}
 				}
 			}
-
 			return true
 		})
 	}
--- testdata/src/testlintdata/keystyle/logdata.go
+++ testdata/src/testlintdata/keystyle/logdata.go
@@ -3,16 +3,24 @@ package todo
 // LogData is the map of which we want to lint the keys.
 type LogData map[string]any
 
+// TODO: ajsdlasj
 func SomeFunc1() {
 	data := LogData{
-		"key1": "value1",
+		"key1":            "value1",
+		"incorrect_key_1": "value2",
 	}
 	_ = data["key1"]
+	_ = someData
+	_ = somePointerData
 }
 
 var (
 	someData = LogData{
-		"camelCase":  "value1",
-		"PascalCase": "value2",
+		"camelCase":     "value1",
+		"IncorrectKey2": "value2",
+	}
+	somePointerData = &LogData{
+		"camelCase":       "value3",
+		"incorrect-key-3": "value4",
 	}
 )
